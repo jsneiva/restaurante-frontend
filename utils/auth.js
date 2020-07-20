42e1497d4edf5bd725a7c5a3bbdac437dc38ec0c
josenilson <josenilson.neiva@gmail.com>
2020-07-20 15:17:08 -0300

    Melhorias

--- utils/auth.js
+++ utils/auth.js
@@ -4,15 +4,11 @@ export async function userLoggedSSR(ctx) {
   let user = null
   try {    
     const token = ctx.req.headers.cookie.replace(/(?:(?:^|.*;\s*)token\s*\=\s*([^;]*).*$)|^.*$/, "$1")
-    if (!token) {
-      throw new Error('Token não encontrado!')
-    }
     axios.defaults.headers.Authorization = 'Bearer ' + token
     const resp = await axios.get('/users/profile')
     user = resp.data
   } catch (error) {
-    ctx.res.json({mensagem: error})
-    //ctx.res.writeHead(302, { Location: '/login' })
+    ctx.res.writeHead(302, { Location: '/' })
     ctx.res.end()
   }
   return user
